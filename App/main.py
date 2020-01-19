f51689553b9c56dc0ba6def1d27b229f967f84ba
ApppleTurnover <apppleturnover@gmail.com>
2020-01-19 10:40:10 +1000

    Secound commit, nothing works(made headers, imports), improved slightly

--- App/main.py
+++ App/main.py
@@ -1,5 +1,3 @@
 import requests
 from bs4 import BeautifulSoup as bs
-from data.headers import *
-
-#https://hh.ru/search/vacancy?area=1&text=python&page=0
+from data.data import Data # Просто класс, в котором хранятся все данные, лул
